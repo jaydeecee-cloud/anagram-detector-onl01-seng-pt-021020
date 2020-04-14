3633c25e7d91af7502a9cc158318d34f505c4d5c
Joseph Choi <jdc07@live.com>
2020-04-14 19:34:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -18,16 +18,3 @@ end
 listen = Anagram.new("listen")
 #listen.match(%w(enlists))
 listen.match(%w(enlists google inlets banana))
- 
-# => ["inlets"]
-
-#test = %w(enlists google inlets banana)
-#word = "listen"
-
-
-#if (word.split("").sort) == (test[2].split("").sort)
-#  puts "test is true"
-#else
-#  puts "test is false"
-#end
-
